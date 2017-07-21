ecedfd09551d5df16958630dfb4bc62b23f7de9b
Martin Panevski <martin.panevski38@gmail.com>
2017-07-21 17:39:34 +0200

    login with fb

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique()->nullable();
+            $table->string('password');
+            $table->string('fb_id');
+            $table->string('fb_link');
+            $table->string('fb_avatar');
+            $table->string('fb_token');
+            $table->string('gender');
+            $table->boolean('verified');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
