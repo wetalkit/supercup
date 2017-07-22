5e1c298248b2ebfa6effb4ba28a66badc2dce2b6
Katerina Trajchevska <trajchevska@adevait.com>
2017-07-22 19:57:03 +0200

    listing creation implementation

--- app/Listing.php
+++ app/Listing.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Listing extends Model
 {
-    //
+    protected $table = 'listings';
+    protected $guarded = ['id'];
+
+    public function pictures()
+    {
+        return $this->hasMany(ListingPictures::class);
+    }
+
+    public function setTermsAcceptedAttribute($value)
+    {
+        $this->attributes['terms_accepted'] = $value == 'on' ? 1 : 0;
+    }
 }
