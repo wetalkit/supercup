9f9a7f5537deccf0a350539fabc29fcafac5e564
Tosho Trajanov <toso.trajanov@gmail.com>
2017-07-24 01:36:35 +0200

    Update content

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -14,7 +14,7 @@
 						<p>Awesome residents from Macedonia post their hostings and offer you their warm home and a bed for you or your friends. This way, you can spend all your money on something more valuable, like beer. 🍺 If you are a Macedonian or live in Macedonia, you can join this website and offer a bed or two in your home for fans who are coming to Skopje for the match.. You will help us show that we still are the welcoming and fun people we have always been. 😎</p>
 
 						<p>---------------------</p>
-						<p>About our community: WeTalkIT is a community, consisted of a bunch of techies who educate each other by sharing and implementing ideas that make a difference in our cool nerdy lives.
+						<p><b>About our community:</b> WeTalkIT is a community, consisted of a bunch of techies who educate each other by sharing and implementing ideas that make a difference in our cool nerdy lives.
 						</p>
                    </div>
                </div>
@@ -25,13 +25,15 @@
                </h5>
                <div class="faq-content">
                    <div class="faq-toggle">
-                       <p>We are a group of young IT and marketing professionals which came together in a WeTalkIT community in order to make positive changes in the society.</p>  
+                       <p>We are a group of young IT and marketing professionals which came together in a WeTalkIT community in order to make positive changes in the society.  </p>  
 
 						<p>Our city Skopje had the luck of the draw to be the host of the UEFA Super Cup 2017, our country first UEFA club competition, scheduled for August 8 in the city stadium, National Arena Filip II. Everything started so great with all of us being proud for this opportunity. Hosting a tournament in Skopje between such great teams like Real Madrid and Manchester United doesn’t happen every day.</p>
 
-						<p>But right after the announcement, things went crazy in our country. We have many places where people can stay, but instead showing our warm hospitality, many of us started being greedy. Local hotel owners jumped on the bandwagon and Skopje accommodation prices skyrocketed. On regular days, one night stay in Skopje would cost you between 20 and 60 euros, and although the increase of price is normal in these circumstances, the costs went to the roof, broke it several times and now are similar to a night on the Maldives. This was unacceptable for us.</p>
+						<p>But right after the announcement, things went crazy in our country. We have many places where people can stay, but instead showing our warm hospitality, many of us started being greedy. Local hotel owners jumped on the bandwagon and <b>Skopje accommodation prices skyrocketed</b>. On regular days, one night stay in Skopje would cost you between 20 and 60 euros, and although the increase of price is normal in these circumstances, the costs went to the roof, broke it several times and now are similar to a night on the Maldives. <b>This was unacceptable for us</b>.</p>
 
-						<p>Suddenly after the scandal, many Macedonians over the social networks offered FREE accommodation for the fans who got tickets for the match. That’s when it clicked us, why don’t we use our IT and marketing skills to create a positive social impact?  It as the exact moment when We Talk IT decided to create this platform.So, this is where our community came forward offering foreign fans to find FREE place to stay for a night or two. If you need a place to crash,, check the FAQ page how can you do that here.</p>
+						<p>Suddenly after the scandal, many Macedonians over the social networks offered <b>FREE accommodation</b> for the fans who got tickets for the match. <b>That’s when it clicked us, why don’t we use our IT and marketing skills to create a positive social impact?</b></p>
+
+            <p>This was the exact moment when We Talk IT members decided to create this platform <b>offering foreign fans to find FREE place for a night or two.</b> So, if you need a place to crash, check the FAQ page or search the listings right away!</p>
 
 		                <p><b>We hope that you will choose to stay in Skopje area and enjoy the Super Cup 2017!🤞</b></p>
 
