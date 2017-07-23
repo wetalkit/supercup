9a35975727ae5d6c1e83f4448f66d144e7a12c12
Katerina Trajchevska <trajchevska@adevait.com>
2017-07-24 01:57:49 +0200

    fix fb share error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,7 @@
       var js, fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) return;
       js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=274999166309959";
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId={{env('FB_CLIENT')}}";
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));</script>
     <header>
