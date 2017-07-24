58a9781a61f158b2492686e33bd04edaa1c1f381
Katerina Trajchevska <trajchevska@adevait.com>
2017-07-24 11:46:17 +0200

    cut title on homepage to fit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -64,7 +64,7 @@
                     <div class="gallery-section {{$listing->status ? 'booked' : ''}}" style="background-image: url({!! $listing->defaultImageSrc ? route('storage', $listing->defaultImageSrc) : '/images/imagena.jpg' !!})">
                         <span class="label label-booked">Booked</span>
                     </div>
-                    <h3 class="listing-author">{{ $listing->title }}</h3>
+                    <h3 class="listing-title">{{ $listing->title }}</h3>
                     <h3 class="listing-author">by {!! $listing->user->name !!}</h3>
                     <span class="distance">{!! $listing->distanceFormatted !!} from Stadium</span>
                     <span>-</span>
