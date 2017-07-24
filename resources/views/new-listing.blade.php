28546ee06235f8e92f693374ed5f61ff45138c9d
Katerina Trajchevska <trajchevska@adevait.com>
2017-07-24 03:27:48 +0200

    ixes

--- resources/views/new-listing.blade.php
+++ resources/views/new-listing.blade.php
@@ -2,11 +2,13 @@
 
 @section('content')
 <div class="container">
-    <div class="wrapper listingform heading">
-        <h1>Hi, {{Auth::user()->name}}! Let's get started listing your space.</h1>
-        {!! Form::open(['route' => ['listing.store'], 'class' => 'form-horizontal', 'enctype' => 'multipart/form-data']) !!}
-        @include('_listing-form')
-        {!!Form::close()!!}
+    <div class="wrapper listingform">
+        <div class="heading">
+            <h1>Hi, {{Auth::user()->name}}! Let's get started listing your space.</h1>
+            {!! Form::open(['route' => ['listing.store'], 'class' => 'form-horizontal', 'enctype' => 'multipart/form-data']) !!}
+            @include('_listing-form')
+            {!!Form::close()!!}
+        </div>
     </div>
 </div>
 @endsection
